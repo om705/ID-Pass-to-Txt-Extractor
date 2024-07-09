4866730b3db32fc2cc09ee46d89c7f31385c8e3e
om705 <61368509+om705@users.noreply.github.com>
2024-07-09 21:15:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 api_id = 22114233
 api_hash = "d7abcec5c967414fadb1d438fa05ebea"
-bot_token = "7260371096:AAGO_SFqKssldaRopGC150qV9XyQwcLOZNQ"
+bot_token = "7323971535:AAG9bGfZXNUVSwLolBVA6VDtOXaCt7SJErs"
 auth_users = "1403488629"
-sudo_users = [int(num) for num in auth_users.split(",")]
+sudo_users = [int(num) for num in auth_users.split("1403488629")]
 osowner_users = "1403488629"
-owner_users = [int(num) for num in osowner_users.split(",")]
+owner_users = [int(num) for num in osowner_users.split("1403488629")]
